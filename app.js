dfcb141d426e6c9b93fa9d0571128ba3a5d28005
Anurag Peshne <anpeshne@microsoft.com>
2019-04-17 18:59:33 -0700

    adds docker file

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var ExpressPeerServer = require('peer').ExpressPeerServer;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -16,7 +17,7 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger('prod'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -24,6 +25,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+// PeerJS setup
+const server = app.listen(9000, '127.0.0.1');
+const peerserver = ExpressPeerServer(server, { debug: true });
+app.use('/peerjs', peerserver);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- bin/startPeer.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-$DIR/../node_modules/peer/bin/peerjs --port 9000 --key peerjs
